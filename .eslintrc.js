a48e96bfbf155739913d71cdbd384c91996b41e4
Yulia <Yulia159V@gmail.com>
2022-05-11 17:16:29 +0300

    fix: mistakes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,12 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
+    "extends": "airbnb-base",
     "parserOptions": {
         "ecmaVersion": "latest"
     },
     "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
     }
 }
